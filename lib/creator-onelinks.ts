e829f0416a16467607439c7aece39ce691ea3abc
Neil Shah <shahjneil@gmail.com>
2026-06-22 22:56:31 -0400

    Update creator links

--- lib/creator-onelinks.ts
+++ lib/creator-onelinks.ts
@@ -1,10 +1,10 @@
 export const CREATOR_ONELINKS = {
-  '/pedpatrol': 'https://predator.onelink.me/RpE0/8jjt6f2t',
-  '/huntforpreds': 'https://predator.onelink.me/RpE0/rexk1nss',
-  '/highervids': 'https://predator.onelink.me/RpE0/heiwqqh4',
-  '/catchemonlive': 'https://predator.onelink.me/RpE0/d8mz0963',
-  '/nypredhunters': 'https://predator.onelink.me/RpE0/a4iku9xx',
-  '/beatingpedos': 'https://predator.onelink.me/RpE0/lk8c6yod',
+  '/pedpatrol': 'https://appstack.link/EhzLFnmC?media_source=Influencers&campaign_name=pedpatrol&campaign_id=3ea08afe-707f-4faa-b224-611001784f94',
+  '/huntforpreds': 'https://appstack.link/DZOYF5tU?media_source=Influencers&campaign_name=huntforpreds&campaign_id=08ae533b-deeb-43cb-9b85-ee0ec00a6679',
+  '/highervids': 'https://appstack.link/abElohVc?media_source=Influencers&campaign_name=highervids&campaign_id=2bb67f34-2646-4acd-9aee-9c427225ff3c',
+  '/catchemonlive': 'https://appstack.link/U7znX4R9?media_source=Influencers&campaign_name=catchemonlive&campaign_id=48009ab5-25e5-4982-a723-aceb7059b012',
+  '/nypredhunters': 'https://appstack.link/U8mJKcyy?media_source=Influencers&campaign_name=nypredhunters&campaign_id=e8c4a93b-ddb1-4380-9618-0a9535a3b52f',
+  '/beatingpedos': 'https://appstack.link/NsgTJRBL?media_source=Influencers&campaign_name=beatingpedos&campaign_id=e109e31d-7911-43dd-a25b-356bd21893fb',
 } as const;
 
 export type CreatorPath = keyof typeof CREATOR_ONELINKS;
